df597abfa4ffe0b459b3dc2e038324d691822011
leekheang <ly.kheang.jr@gmail.com>
2019-11-03 00:30:48 +0700

    mutation default_value

--- schema.py
+++ schema.py
@@ -1,11 +1,13 @@
 import graphene
+import uuid
 import json
 from datetime import datetime
 
 class User(graphene.ObjectType):
-    id = graphene.ID()
+    id = graphene.ID(default_value=uuid.uuid4())
     username = graphene.String()
-    created_at = graphene.DateTime()
+    created_at = graphene.DateTime(default_value=datetime.now())
+
 
 class Query(graphene.ObjectType):
     users = graphene.List(User , limit=graphene.Int())
@@ -23,17 +25,35 @@ class Query(graphene.ObjectType):
             User( id="1", username="leekheang", created_at=datetime.now()),
             User( id="2", username="nat", created_at=datetime.now())
         ][:limit]
-schema = graphene.Schema(query=Query)
+
+class Create_User(graphene.Mutation):
+    user = graphene.Field(User)
+    
+    class Arguments:
+        username = graphene.String()
+    
+    def mutate(self, info ,username):
+        user = User(username=username)
+        return Create_User(user=user)
+
+class Mutation(graphene.ObjectType):
+    create_user = Create_User.Field()
+
+
+
+schema = graphene.Schema(query=Query , mutation=Mutation)
 
 result = schema.execute(
     '''
-      {
-          users {
-                id
-                username
-                createdAt
-          }
-      }
+        mutation {
+            createUser(username:  "yong"){
+               user {
+                    id
+                    username
+                    createdAt
+               }
+            }
+        }
     '''
 )
 
